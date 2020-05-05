77162a452e04a640b70814290a2659241ff8fb1a
Takashi Kitajima <inc@2inc.org>
2020-05-06 08:32:12 +0900

    0.5.3

--- snow-monkey-diet.php
+++ snow-monkey-diet.php
@@ -2,7 +2,7 @@
 /**
  * Plugin name: Snow Monkey Diet
  * Description: You can stop unused functions of the Snow Monkey.
- * Version: 0.5.2
+ * Version: 0.5.3
  * Author: inc2734
  * Author URI: https://2inc.org
  * License: GPL2 or later
